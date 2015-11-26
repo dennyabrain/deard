90acaf761175ca00405231d22ab3ae3e472985ad
dennyabrain <denny.george90@gmail.com>
2015-11-25 20:25:22 -0500

    slack code added for testing

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
-import os
-from flask import Flask
+from flask import Flask, request
+import requests
+import json
+
+url = 'https://hooks.slack.com/services/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'
+payload = {'key1': 'value1', 'key2': 'value2','text':'asdfsadf asdf sadf '}
 
 app = Flask(__name__)
+app.secret_key='itp'
 
-@app.route('/')
-def hello():
-    return 'Hello World! 22222222'
+@app.route('/reply', methods=['POST'])
+def reply():
+	if request.method=='POST':
+		print('data received')
+		r = requests.post(url, data=json.dumps({'text':'hearing back from the app'}))
+		return 'data recieved'
