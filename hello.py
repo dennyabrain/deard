42608aff33845bf5eaadf5d0fa8b025b5bd1096f
dennyabrain <denny.george90@gmail.com>
2016-01-07 16:36:41 -0500

    copied app to hello.py

--- hello.py
+++ hello.py
@@ -1,28 +1,38 @@
 from db import db
 import sys
-from flask import Flask, request, redirect, url_for, render_template, jsonify
+from flask import Flask, request, redirect, url_for, render_template, jsonify, session
 import requests
 import json
 import flask.ext.login as flaskLogin
 import afinn
 from datetime import datetime, timedelta, date, time
 import logging
+from ml import ml
+from uuid import uuid4
+from flask.ext.bcrypt import Bcrypt
+from helper import incrementCFT
+from responseHelper import Response
 
 url = 'https://hooks.slack.com/services/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'
 payload = {'key1': 'value1', 'key2': 'value2','text':'asdfsadf asdf sadf '}
 
-#database = db('slack','responseCollection')
 database = db('heroku_lmx991zw','responseCollection')
 databaseUser = db('heroku_lmx991zw','users')
 
+
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-app.secret_key='itp'
 loginManager=flaskLogin.LoginManager()
 loginManager.init_app(app)
+bcrypt = Bcrypt(app)
+
+response=Response()
+
+commentFormType=['greeting','mood','situation','feeling','thought','preMechTurk','review','rethinking','bye']
 
 class User(flaskLogin.UserMixin):
 	pass
@@ -52,34 +62,6 @@ def reply():
 def home():
 	return render_template('index.html')
 
-@app.route('/diary', methods=['POST','GET'])
-@flaskLogin.login_required
-def diary():
-	if request.method=='POST':
-		requests.post(url, data=json.dumps({'text':request.form['text']}))
-		#requests.post(url, data=json.dumps({'text':flaskLogin.current_user.id}))
-		databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'])
-		return redirect(url_for('diary'))
-		#return 'posted'
-	
-	return render_template('diary.html',database=databaseUser.listAll(),database2=databaseUser.listAll())
-
-@app.route('/login',methods=['POST','GET'])
-def login():
-	if request.method=='GET':
-		return render_template('index.html')
-
-	for post in databaseUser.findMany({}):
-		if request.form['username'] in post:
-			if request.form['pw']==post[request.form['username']]['pw']:
-				user = User()
-				user.id=request.form['username']
-				flaskLogin.login_user(user)
-				return redirect(url_for('diary'))
-				#return "Can Log In"
-			#return str(post[request.form['username']]['pw'])
-	return "Username and Password don't match"
-
 @app.route('/logout')
 def logout():
 	flaskLogin.logout_user()
@@ -91,49 +73,113 @@ def register():
 	if request.method=='GET':
 		return render_template('index.html')
 
-		
+	#Check if the user exists:
+	for post in databaseUser.findMany({}):
+		if request.form['username'] in post:
+			return'{"status":"userExists"}'
+
+	#Creating Hashed Password:
+	pw_hash=bcrypt.generate_password_hash(request.form['pw']);
+
 	#databaseUser.insertOne({request.form['username']:{'pw':request.form['pw']}})
-	databaseUser.insertOne({"name":request.form['username'],request.form['username']: {'pw':request.form['pw'],'text':[]}})
+
+	databaseUser.insertOne({"name":request.form['username'],request.form['username']: {'pw':pw_hash,'text':[]}})
 	user = User()
 	user.id=request.form['username']
 	flaskLogin.login_user(user)
-	databaseUser.insertReply(request.form['username'],"Hi, %s. I'm Dee. I'm here whenever you want to talk about your day, and help you keep track of the topics and your mood. How was your day today?" % request.form['username'])
+	session['id']=uuid4()
+	session['index']=1
+	databaseUser.insertReply(request.form['username'],"Hi, %s. I'm Dee. I'm here whenever you want to talk about your day, and help you keep track of the topics and your mood. How was your mood today?" % request.form['username'],session['id'],"mood",0.0)
 	return '{"status":"success"}'
+
+# Get from 30 day range:   /userstats?range=30
+# Get from specified date:   /userstats?startdate=2015-12-01
+# Get 30 days from specified date:   /userstats?range=30&startdate=2015-12-01
 
 @app.route('/userstats', methods=['GET'])
 def userstats():
 	if request.method=='GET':
-		text={"0":[],"1":[],"2":[],"3":[],"4":[],"5":[],"6":[]}
+		# Set the date range (default: 7 days)
+		daterange = int(request.args.get('range', 7))
+		if daterange < 1 or daterange > 90:
+			daterange = 7
+
+		# Set startdate (default: today) and enddate
+		startdate_str = request.args.get('startdate', None)
+		if not startdate_str:
+			startdate = datetime.today()
+		else:
+			startdate = datetime.strptime(startdate_str, '%Y-%m-%d') # 2015-12-31
+		enddate = (startdate - timedelta(days=daterange))
+
+		# Initialize results
+		text = {}
+		for i in range(daterange):
+			text[ str(i) ] = []
+
+		# Get comments from database
 		comments = databaseUser.listAllText(flaskLogin.current_user.id)
-		#print comments
 		for item in comments:
 			temp = []
 			if 'created_at' in item:
-				td=datetime.now()-item['created_at']
-				if td.days<=6:
-					text[str(td.days)].append(item)
+				# Calculate difference based on each day
+				td = ( startdate.date() - item['created_at'].date() ).days
 
-		return jsonify(comments=text)
+				# Ensure comment is in date range, add to array for that day
+				if td < daterange and td >= 0:
+					text[ str(td) ].append(item)
+
+		# Return results
+		return jsonify(comments=text,daterange=daterange,startdate=startdate.strftime('%Y-%m-%d'),enddate=enddate.strftime('%Y-%m-%d'))
 
 @app.route('/comments', methods=['POST','GET'])
 def comment():
 	if request.method=='POST':
-		#print(flaskLogin.current_user.id)
-		#print(request.form['text'])
-		databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'])		
-		requests.post(url, data=json.dumps({'text':str(str(flaskLogin.current_user.id)+' says: ' +str(request.form['text']))}))
+		postId=session['id']
+		if session['index']==1: #MOOD
+			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
+			#HACKY MOOD MAPPING
+			if request.form['text']==':D' or request.form['text']==':)':
+				mood="happy"
+			elif request.form['text']==':/':
+				mood="ok"
+			else:
+				mood="bad"
+
+			session['mood']=mood
+			session['index']=incrementCFT(session['index'])
+			databaseUser.insertReply(flaskLogin.current_user.id,response.getSituation(session['mood']), session['id'], commentFormType[session['index']],0)
+		elif session['index']==2: #SITUATION
+			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
+			session['index']=incrementCFT(session['index'])
+			databaseUser.insertReply(flaskLogin.current_user.id,response.getFeeling(session['mood']), session['id'], commentFormType[session['index']],0)
+		elif session['index']==3: #FEELING
+			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
+			session['index']=incrementCFT(session['index'])
+			databaseUser.insertReply(flaskLogin.current_user.id,response.getThought(session['mood']), session['id'], commentFormType[session['index']],0)
+		elif session['index']==4: #THOUGHT
+			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
+			session['index']=incrementCFT(session['index'])
+			databaseUser.insertReply(flaskLogin.current_user.id,response.getPreMechTurk(session['mood']), session['id'], commentFormType[session['index']],0)
+		elif session['index']==5: #PREMECHTURK
+			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
+			session['index']=incrementCFT(session['index'])
+			databaseUser.insertReply(flaskLogin.current_user.id,"insert mechanicalTurkReponse here", session['id'], commentFormType[session['index']],0)
+		elif session['index']==6: #REVIEW
+			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
+			session['index']=incrementCFT(session['index'])
+			session['review']=request.form['text']
+			databaseUser.insertReply(flaskLogin.current_user.id,response.getReview(session['review']), session['id'], commentFormType[session['index']],0)
+			databaseUser.insertReply(flaskLogin.current_user.id,response.getRethinking(session['review']), session['id'], commentFormType[session['index']],0)
+		elif session['index']==7: #RETHINKING
+			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
+			session['index']=incrementCFT(session['index'])
+			databaseUser.insertReply(flaskLogin.current_user.id,response.getBye(session['mood']), session['id'], commentFormType[session['index']],0)
+
 		"""
-		Trying out simplest sentiment analysis: returns a float score based on text
+		Post Question on mTurk
 		"""
-		text = str(request.form['text'])
-		afinnScore = afinn.sentiment(text)
-		print("******** SENTIMENT SCORE: %6.2f ********** %s" % (afinnScore, text))
-		if afinnScore > 0:
-			databaseUser.insertReply(flaskLogin.current_user.id,"%6.2f That's great!" % (afinnScore), score=afinnScore)
-		elif afinnScore == 0:
-			databaseUser.insertReply(flaskLogin.current_user.id,"%6.2f Gotcha." % (afinnScore), score=afinnScore)
-		else:
-			databaseUser.insertReply(flaskLogin.current_user.id,"%6.2f Sorry to hear :(" % (afinnScore), score=afinnScore)
+
 		return jsonify(status='commentInsert')
 
 	if request.method=='GET':
@@ -153,11 +199,17 @@ def login2():
 	if request.method=='POST':
 		for post in databaseUser.findMany({}):
 			if request.form['userKey'] in post:
-				if request.form['password']==post[request.form['userKey']]['pw']:
+				if bcrypt.check_password_hash(post[request.form['userKey']]['pw'],request.form['password']):
 					user = User()
 					user.id=request.form['userKey']
 					flaskLogin.login_user(user)
-					databaseUser.insertReply(request.form['userKey'],"Hey, %s. How's it going?" % request.form['userKey'])
+					# CREATE A NEW SESSION ID ASSOCIATED WITH THIS USER
+					session['id']=uuid4()
+					session['index']=1
+					databaseUser.insertReply(request.form['userKey'],"Hey, %s. How's it going?" % request.form['userKey'], session['id'],"greeting",0)
+					databaseUser.insertReply(request.form['userKey'],"Good morning. How is your mood today?", session['id'],"mood",0)
+					print ('index')
+					print (session['index'])
 					print ('flask has logged in and user is : ')
 					print (flaskLogin.current_user.id)
 					return '{"status":"success"}'
