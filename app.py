b3adee5fbcb65ae0a35d64fcdb36bae6425b1189
dennyabrain <denny.george90@gmail.com>
2016-01-09 13:18:13 -0500

    changes to emit

--- app.py
+++ app.py
@@ -152,13 +152,13 @@ def comment():
 			session['mood']=mood
 			session['index']=incrementCFT(session['index'])
 			databaseUser.insertReply(flaskLogin.current_user.id,response.getSituation(session['mood']), session['id'], commentFormType[session['index']],0)
-			socket.emit('insert',jsonify(
+			socket.emit('insert',{
 								text=response.getSituation(session['mood']),
 								affin_score=0,
 								created_at=datetime.now(),
 								post_id=session['id'],
 								type='bot', 
-								commentFormType=commentFormType[session['index']]))
+								commentFormType=commentFormType[session['index']]})
 		elif session['index']==2: #SITUATION
 			databaseUser.insertInput(flaskLogin.current_user.id,request.form['text'],session['id'])
 			session['index']=incrementCFT(session['index'])
