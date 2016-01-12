4008b5dabae35d2811cf10fecb5b8080d274c9ea
dennyabrain <denny.george90@gmail.com>
2016-01-12 00:34:43 -0500

    socketio client added

--- hello.py
+++ hello.py
@@ -9,7 +9,11 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def home():
-	return 'Home'
+	return render_template('test.html')
+
+@socketio.on('connect')
+def connect():
+	emit('connected','flask says hi')
 
 if __name__=='__main__':
 	socketio.run(app)
