2aafc279cd687b5e1df9fe75b8d5fa78964d59f7
dennyabrain <denny.george90@gmail.com>
2016-01-20 09:31:57 -0500

    fixed room id problem for preMechTurk stage

--- hello.py
+++ hello.py
@@ -179,7 +179,7 @@ def comment():
 								'created_at':str(datetime.now()),
 								'post_id':str(session['id']),
 								'type':'bot', 
-								'commentFormType':'preMechTurk'},sid[flaskLogin.current_user.id])
+								'commentFormType':'preMechTurk'},room=sid[flaskLogin.current_user.id])
 			return jsonify(status='commentInsert')
 
 		#print "the request form is ===" 
